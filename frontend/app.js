2dc0ae0fa45dcea87d24b342ae1caf4baaf565a9
Rikatsu17 <daniyaladilbekov@gmail.com>
2026-02-08 14:03:18 +0500

    Changing to sepolia network

--- frontend/app.js
+++ frontend/app.js
@@ -1,7 +1,7 @@
 import { ethers } from "https://cdn.jsdelivr.net/npm/ethers@6.13.5/+esm";
 
-const INVOICEFUND_ADDRESS = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
-const REWARDTOKEN_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+const INVOICEFUND_ADDRESS = "0xbADA66D973aa12c43f06F8f46b846F04f3CC2c7c";
+const REWARDTOKEN_ADDRESS = "0xaC3C1F55973Ea2c1137b3295D52fBB556C11569e";
 
 const invoiceFundAbi = [
     {
@@ -737,6 +737,16 @@ el("connectBtn").onclick = async () => {
     setStatus("Requesting account access...");
     provider = new ethers.BrowserProvider(window.ethereum);
     await provider.send("eth_requestAccounts", []);
+    const SEPOLIA_CHAIN_ID = 11155111;
+
+    const network = await provider.getNetwork();
+    const chainId = Number(network.chainId);
+    console.log("Detected chainId:", chainId);
+
+    if (chainId !== SEPOLIA_CHAIN_ID) {
+      setStatus("Please switch MetaMask to Sepolia network");
+      return;
+    }
     signer = await provider.getSigner();
     userAddress = await signer.getAddress();
     el("addr").textContent = userAddress;
